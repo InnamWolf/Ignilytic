b9766ce7470280802a94130fc4e3eda811536685
VMRR <m.ramirez@ignilytic.com>
2023-01-27 18:14:58 -0600

    Se crea menu movil/desktop

---
+++ view/components/menu.php
@@ -0,0 +1,48 @@
+<!--===============================================
+Nav movil
+=================================================-->
+<nav class="movil">
+
+  <div class="movil__topNav">
+    <i class="fa fa-bars menu" id="menu" aria-hidden="true"></i>
+    <div class="movil__logo">
+      <i class="fa-regular fa-moon modo"></i>
+      <a href="inicio"><img src="view/src/img/logo.svg" alt="logo"></a>
+    </div>
+  </div>
+
+  <div class="movil__categorias">
+    <a href="inicio"><h3>Inicio</h3></a>
+    <a href="servicios"><h3>Servicios</h3></a>
+    <a href="portafolio"><h3>Portafolio</h3></a>
+    <a href="faq"><h3>FAQ</h3></a>
+    <a href="Contacto" ><h3>Contacto</h3></a>
+    <a href="#" class='movil__btn'>Inicia Sesión</a>
+  </div>
+
+</nav>
+
+<!--===============================================
+Nav Desktop
+=================================================-->
+<nav class="escritorio">
+
+  <div class="escritorio__navegacion">
+
+    <div class="escritorio_logo"><a href="inicio"><img src="view/src/img/logo.svg" alt="logo"></a></div>
+
+    <div class="escritorio__categorias">
+      <a class='escritorio__item' href="inicio">Inicio</a>
+      <a class='escritorio__item' href="servicios">Servicios</a>
+      <a class='escritorio__item' href="portafolio">Portafolio</a>
+      <a class='escritorio__item' href="faq">FAQ</a>
+      <a class='escritorio__item' href="Contacto">Contacto</a>
+      <i class="fa-regular fa-moon modoDesk"></i>
+      <a href="#" class='escritorio__btn'>Inicia Sesión</a>
+    </div>
+
+  </div>
+
+</nav>
+
+
--- view/template.php
+++ view/template.php
@@ -8,25 +8,19 @@
   <!--===============================================
   Lib CSS
   =================================================-->
-  <link rel="stylesheet" href="view/css/main.css">
+  <link rel="stylesheet" href="view/src/css/main.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
-  <!-- Boostrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Carrusel -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css"/>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"/>
+  <!-- Normalize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"/>
+
   <!--===============================================
   lib js
   =================================================-->
-  <!-- JavaScript Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Jquery -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <!-- Carousel -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 
-  <link rel="icon" href="view/img/icono.png" sizes="64x64">
+  <link rel="icon" href="view/src/img/icono.png" sizes="64x64">
 
   <title>Ignilytic - TI</title>
 
@@ -47,23 +41,14 @@
       //* ===============================================
       if(isset($_GET["url"])){
 
-        if($_GET["url"] == "inicio" ||
-          $_GET["url"] == "servicios" ||
-          $_GET["url"] == "web" ||
-          $_GET["url"] == "ti" ||
-          $_GET["url"] == "polizas" ||
-          $_GET["url"] == "diseno" ||
-          $_GET["url"] == "aplicaciones" ||
-          $_GET["url"] == "portafolio" ||
-          $_GET["url"] == "faq" ||
-          $_GET["url"] == "contacto"
+        if($_GET["url"] == "inicio"
+
         ){
 
           include "pages/".$_GET["url"].".php";
 
         }else{
-
-            include "pages/404.php";
+            // include "pages/404.php";
         }
 
       }else{
@@ -72,12 +57,12 @@
       //* ===============================================
       //* Footer
       //* ===============================================
-      include_once 'view/components/footer.php';
+      // include_once 'view/components/footer.php';
     ?>
 
   </div>
 
-  <script src="view/js/main.js"></script>
+  <script src="view/src/js/main.js"></script>
 
 </body>
 
